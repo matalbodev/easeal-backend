420eb5341fd38ebbea000f2a447b057aacc8339c
madmat <m.albore@noiise.com>
2023-02-20 15:19:08 +0100

    Update switch mac change

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,43 @@
 import { PrismaClient } from '@prisma/client';
+import axios from 'axios';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.ingredient.create({
-    data: {
-      name: 'Pasta',
-      kcal: 400,
-      mainType: 'carbohydrate',
-    },
+// remove all ingredients before seeding
+async function removeIngredients() {
+  const ingredients = await prisma.ingredient.findMany();
+  ingredients.forEach(async (ingredient) => {
+    await prisma.ingredient.delete({
+      where: {
+        id: ingredient.id,
+      },
+    });
   });
-  await prisma.ingredient.create({
-    data: {
-      name: 'Tomato sauce',
-      kcal: 100,
-      mainType: 'carbohydrate',
-    },
+}
+
+async function main() {
+  await removeIngredients();
+
+  // axios get request to get the data from the API
+  const response = await axios.get('https://fruityvice.com/api/fruit/all');
+  const fruits = response.data;
+
+  // loop through the data and create a new ingredient for each fruit
+  fruits.forEach(async (fruit) => {
+    const nutritions = fruit.nutritions;
+    const nutritionsClone = { ...nutritions };
+    delete nutritionsClone.calories;
+    const highestNutrition = Object.keys(nutritionsClone).reduce((a, b) =>
+      nutritionsClone[a] > nutritionsClone[b] ? a : b,
+    );
+
+    await prisma.ingredient.create({
+      data: {
+        name: fruit.name,
+        kcal: fruit.nutritions.calories,
+        mainType: highestNutrition,
+      },
+    });
   });
 }
 
